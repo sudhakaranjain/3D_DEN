1637508b3d05491cb8789dbb72fd76da2591715a
sudhakaranjain <sudhakaran.cj@gmail.com>
2020-04-15 20:32:41 +0530

    working incremental model for fruit

--- 2d_recognition/2d_recog.py
+++ 2d_recognition/2d_recog.py
@@ -8,6 +8,7 @@ class DEN():
 
 	def __init__(self):
 		self.last_label_index = 0
+		self.params = dict()
 
 	def extract_data(self, filepath):
 		data = []
@@ -20,7 +21,7 @@ class DEN():
 			for img in os.listdir(label_path):
 				image_path = os.path.join(label_path, img)
 				image = cv.imread(image_path)
-				re_image = cv.resize(image, (50,50))
+				re_image = cv.resize(image, (60,60))
 				# grayImage = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 				# (thresh, BW) = cv.threshold(grayImage, 127, 255, cv.THRESH_BINARY)
 				data.append(re_image)
@@ -34,7 +35,7 @@ class DEN():
 		return data, data_labels, label_names
 
 
-	def add_task(self, label_names, binary_output=True):
+	def add_task(self, task_id, label_names, initial_output=2):
 
 
 		new_label_indices = []
@@ -43,15 +44,14 @@ class DEN():
 		new_test = []
 		new_test_labels = []
 
-		if binary_output == False:
-			new_label_indices.append(label_names[self.last_label_index+1])
-			self.last_label_index = self.last_label_index + 1
-
-		else:
-			for i in range(no_of_labels):
-				new_label_indices.append(label_names[i])
+		if task_id == 1:
+			for i in range(initial_output):
+				new_label_indices.append(i)
 			self.last_label_index = i
 
+		else:
+			new_label_indices.append(self.last_label_index+1)
+			self.last_label_index = self.last_label_index + 1
 
 		for index in new_label_indices:
 			for data, label in zip(train_data, train_labels):
@@ -67,13 +67,19 @@ class DEN():
 		return [np.array(new_train), np.array(new_train_labels), 
 					np.array(new_test), np.array(new_test_labels)]
 
-	def weight_variable(self, shape):
-		initial = tf.truncated_normal(shape, stddev=0.1)
-		return tf.Variable(initial)
+	def weight_variable(self, task_id, shape, scope=None, trainable=True):
+		with tf.variable_scope(scope, reuse=True):
+			w = tf.get_variable(task_id, shape, trainable=trainable)
+			self.params[w.name] = w
+			
+		return w
 
-	def bias_variable(self, shape):
-		initial = tf.constant(0.1, shape=shape)
-		return tf.Variable(initial)
+	def bias_variable(self, shape, scope=None, trainable=True):
+		if den == False
+			initial = tf.constant(0.1, shape=shape)
+			return tf.Variable(initial)
+		else:
+
 
 	def conv2d(self, x, W):
 		return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
@@ -82,52 +88,55 @@ class DEN():
 		return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
 							strides=[1, 2, 2, 1], padding='SAME')
 
-	def create_model(self, x, keep_prob, prev_y_conv=None):
+	def build_model(self, task_id, x, keep_prob, retraining=False, expansion=False, splitting=False):
 
 		tf.reset_default_graph()
 
-		#First Convolutional Layer
-		W_conv1 = self.weight_variable([5, 5, 1, 32])
-		b_conv1 = self.bias_variable([32])
+		if task_id == 1:
 
-		h_conv1 = tf.nn.relu(self.conv2d(x, W_conv1) + b_conv1)
-		h_pool1 = self.max_pool_2x2(h_conv1)
+			#First Convolutional Layer
+			W_conv1 = self.weight_variable(task_id, [5, 5, 3, 32])
+			b_conv1 = self.bias_variable(task_id, [32])
 
-
-		#Second Convolutional Layer
-		W_conv2 = self.weight_variable([5, 5, 32, 64])
-		b_conv2 = self.bias_variable([64])
-
-		h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
-		h_pool2 = self.max_pool_2x2(h_conv2)
+			h_conv1 = tf.nn.relu(self.conv2d(x, W_conv1) + b_conv1)
+			h_pool1 = self.max_pool_2x2(h_conv1)
 
 
-		#flattened first fc layer
-		W_fc1 = self.weight_variable([7 * 7 * 64, 512])
-		b_fc1 = self.bias_variable([512])
+			#Second Convolutional Layer
+			W_conv2 = self.weight_variable(task_id, [5, 5, 32, 64])
+			b_conv2 = self.bias_variable(task_id, [64])
+
+			h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
+			h_pool2 = self.max_pool_2x2(h_conv2)
 
 
-		h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-		h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+			#flattened first fc layer
+			W_fc1 = self.weight_variable(task_id, [15 * 15 * 64, 1024])
+			b_fc1 = self.bias_variable(task_id, [1024])
 
-		#Dropout Layer
-		h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-		#second fc layer
-		W_fc2 = self.weight_variable([512, 128])
-		b_fc2 = self.bias_variable([128])
+			h_pool2_flat = tf.reshape(h_pool2, [-1, 15*15*64])
+			h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-		h_fc2 = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+			#Dropout Layer
+			h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-		if prev_y_conv == None:
+			#second fc layer
+			W_fc2 = self.weight_variable(task_id, [1024, 128],"den_layer")
+			b_fc2 = self.bias_variable(task_id, [128],"den_layer")
+
+			h_fc2 = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
 			# readout fc layer
-			W_fc3 = self.weight_variable([128, 2])
-			b_fc3 = self.bias_variable([2])
+			W_fc3 = self.weight_variable(task_id, [128, 2])
+			b_fc3 = self.bias_variable(task_id, [2])
+
+			y_conv = tf.matmul(h_fc2, W_fc3) + b_fc3
+
+			return y_conv
+
 		else:
 
-		y_conv = tf.matmul(h_fc2, W_fc3) + b_fc3
-
-		return y_conv
 
 if __name__ == "__main__":
 
@@ -173,23 +182,20 @@ if __name__ == "__main__":
 	# cv.imshow("", train_data[512])
 	# cv.waitKey(0)
 
-	x = tf.placeholder(tf.float32, [None, 50, 50, 3])
+	x = tf.placeholder(tf.float32, [None, 60, 60, 3])
 	y_ = tf.placeholder(tf.float32, [None, 2])
 	keep_prob = tf.placeholder(tf.float32)
-
+	y_conv = None
+	retraining = False
+	expansion = False
+	splitting = False
 
 	for task_id in range(1, len(label_names)):
 
-		prev_y = y_conv
-
-
 		if task_id == 1:
-			[task_train, task_train_labels, task_test, task_test_labels] = den.add_task(label_names)
-			y_conv = den.create_model(task_id, x, keep_prob)
+			y_conv = den.build_model(task_id, x, keep_prob)
 
-		else:
-			task_train , task_labels = den.add_task(label_names, False)
-			y_conv = den.create_model(task_id, x, keep_prob, prev_y)			
+		[task_train, task_train_labels, task_test, task_test_labels] = den.add_task(task_id, label_names)			
 
 		dataset = tf.data.Dataset.from_tensor_slices((task_train task_labels))
 		dataset = dataset.repeat().batch(batch_size)
@@ -197,23 +203,23 @@ if __name__ == "__main__":
 		(x_data , y_data) = iterator.get_next()
 
 
-	with tf.Session() as sess:
+		with tf.Session() as sess:
 
-		cross_entropy = tf.reduce_mean(
-			tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=y_conv))
-		train_model = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
-		correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-		accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-		sess.run(tf.global_variables_initializer())
-		test_labels = sess.run(test_labels)
+			cross_entropy = tf.reduce_mean(
+				tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=y_conv))
+			train_model = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
+			correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+			accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+			sess.run(tf.global_variables_initializer())
+			test_labels = sess.run(test_labels)
 
-		for i in range(epochs):
+			for i in range(epochs):
 
-			for j in range(int(len(train_data)/batch_size)):
-					x_batch , y_batch = sess.run([x_data,y_data])
-					sess.run(train_model, feed_dict={x: x_batch, y_: y_batch, keep_prob: 0.5})
-					train_accuracy = accuracy.eval(feed_dict={x:x_batch, y_: y_batch, keep_prob: 1.0})
-					print("step %d, training accuracy %g"%(i, train_accuracy))
+				for j in range(int(len(train_data)/batch_size)):
+						x_batch , y_batch = sess.run([x_data,y_data])
+						sess.run(train_model, feed_dict={x: x_batch, y_: y_batch, keep_prob: 0.5})
+						train_accuracy = accuracy.eval(feed_dict={x:x_batch, y_: y_batch, keep_prob: 1.0})
+						print("step %d, training accuracy %g"%(i, train_accuracy))
 
-			print("-----------------------test accuracy %g---------------------------"%accuracy.eval(feed_dict={
-				x: test_data, y_: test_labels, keep_prob: 1.0}))
+				print("-----------------------test accuracy %g---------------------------"%accuracy.eval(feed_dict={
+					x: test_data, y_: test_labels, keep_prob: 1.0}))
